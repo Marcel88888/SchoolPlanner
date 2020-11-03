c050065d05089749b893ca0482c6b3a097f16192
Marcel88888 <marcel.kawski1@gmail.com>
2020-11-03 22:55:55 +0100

    Model Lesson renamed, Reader changed and new data to json file added

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -1,14 +1,14 @@
 using System;
 
 namespace SchoolPlanner.Models {
-    public class Activity {
+    public class Lesson {
         public Classroom Classroom { get; set; }
         public _Class _Class { get; set; }
         public string Subject { get; set; }
         public int Slot { get; set; }
         public Teacher Teacher { get; set; }
 
-        public Activity (Classroom classroom, _Class _class, string subject, int slot, Teacher teacher) {
+        public Lesson (Classroom classroom, _Class _class, string subject, int slot, Teacher teacher) {
             Classroom = classroom;
             _Class = _class;
             Subject = subject;
--- Models/Reader.cs
+++ Models/Reader.cs
@@ -10,17 +10,19 @@ namespace SchoolPlanner.Models {
         public List<_Class> Classes { get; set; }
         public List<string> Subjects { get; set; }
         public List<Teacher> Teachers { get; set; }
+        public List<Lesson> Lessons { get; set; }
         public string Chosen_class { get; set; }
 
         public Reader() {
 
-            string jsonFilePath = "/home/dell/Studia/sem6/NTR/projects/SchoolPlanner/data/data.json";
+            string jsonFilePath = "./data/data.json";
             string json = File.ReadAllText(jsonFilePath);
 
             Classrooms = new List<Classroom>();
             Classes = new List<_Class>();
             Subjects = new List<string>();
             Teachers = new List<Teacher>();
+            Lessons = new List<Lesson>();
 
             using JsonDocument doc = JsonDocument.Parse(json);
             JsonElement data = doc.RootElement;
@@ -29,23 +31,42 @@ namespace SchoolPlanner.Models {
             var classes = data.GetProperty("classes");
             var subjects = data.GetProperty("subjects");
             var teachers = data.GetProperty("teachers");
+            var lessons = data.GetProperty("lessons");
             
-            for(int i = 0; i<classrooms.GetArrayLength(); i++) {
+            for(int i=0; i<classrooms.GetArrayLength(); i++) {
                 int classroom = Int16.Parse(classrooms[i].GetString());
                 Classrooms.Add(new Classroom(classroom));
             }
 
-            for(int i = 0; i<classes.GetArrayLength(); i++) {
+            for(int i=0; i<classes.GetArrayLength(); i++) {
                 Classes.Add(new _Class(classes[i].GetString()));
             }
 
-            for(int i = 0; i<subjects.GetArrayLength(); i++) {
+            for(int i=0; i<subjects.GetArrayLength(); i++) {
                 Subjects.Add(subjects[i].GetString());
             }
 
-            for(int i = 0; i<teachers.GetArrayLength(); i++) {
+            for(int i=0; i<teachers.GetArrayLength(); i++) {
                 Teachers.Add(new Teacher(teachers[i].GetString()));
             }
+
+            Console.WriteLine(lessons);
+            var cl_room = Int16.Parse(lessons[0].GetProperty("classroom").GetString());
+            Console.WriteLine(cl_room);
+            /*
+
+            for(int i=0; i<lessons.GetArrayLength(); i++) {
+                var cl_room = Int16.Parse(lessons[i].GetProperty("classroom").GetString());
+                for(int j=0; j<classrooms.GetArrayLength(); j++) {
+                    int cl_room2 = Classrooms[j].Number;
+                    if (cl_room == cl_room2) {
+                        Classroom found_classroom = Classrooms[j];
+                        break;
+                    }
+                }   
+                //Lessons.Add();
+            }
+            */
         }
     }
 }
