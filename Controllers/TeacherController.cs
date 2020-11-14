7f1007438f8d57b9cb51db625fddb85046f71a1d
Marcel88888 <marcel.kawski1@gmail.com>
2020-11-14 16:17:12 +0100

    Adding and editing lessons in Teacher section implemented

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -21,12 +21,46 @@ namespace SchoolPlanner.Controllers {
         }
 
         [HttpPost]
-        public IActionResult Index(Reader reader) {
-            ViewData["chosen_teacher"] = reader.ChosenTeacher;
+        public IActionResult Index(Reader reader) {     // TODO: add "string _classroom" as a parameter 
+            ViewData["all_lessons"] = reader.Lessons;
             ViewData["chosen_lessons"] = reader.getLessonsByTeacher(reader.ChosenTeacher);
             return View(reader);
         }
 
+        public IActionResult AddLesson(Reader reader, int slot, string chosenTeacher) {
+            ViewData["slot"] = slot;
+            ViewData["chosen_teacher"] = chosenTeacher;
+            return View(reader);
+        }
+
+        public IActionResult SubmitAddingLesson(Reader reader, int slot, string chosenTeacher) {
+            reader.NewLesson.Teacher = chosenTeacher;
+            reader.NewLesson.Slot = slot;
+            reader.Lessons.Add(reader.NewLesson);
+            reader.updateJsonFile();
+            return RedirectToAction("Index");       // TODO: Index with teacher
+        }
+
+        public IActionResult EditLesson(Reader reader, int id) {
+            ViewData["lessonToEditIndex"] = id;
+            return View(reader);
+        }
+
+        [HttpPost]
+        public IActionResult SubmitEditingLesson(Reader reader, Lesson editedLesson, int id) {
+            reader.Lessons[id].Classroom = editedLesson.Classroom;
+            reader.Lessons[id].Class = editedLesson.Class;
+            reader.Lessons[id].Subject = editedLesson.Subject;
+            reader.updateJsonFile();
+            return RedirectToAction("Index");
+            // TODO: return RedirectToAction("Index", new { _class = reader.Lessons[id].Class }); 
+        }
+
+        public IActionResult DeleteLesson(Reader reader, int id) {
+            reader.Lessons.RemoveAt(id);
+            reader.updateJsonFile();
+            return RedirectToAction("Index");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
