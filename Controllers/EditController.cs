74355ef6f4da1d2d00bca4a9b4a2b64c986e46f0
Marcel88888 <marcel.kawski1@gmail.com>
2020-12-13 00:41:49 +0100

    DB errors handling added

--- Controllers/EditController.cs
+++ Controllers/EditController.cs
@@ -34,64 +34,104 @@ namespace SchoolPlanner.Controllers {
         [HttpPost]
         public IActionResult Index(Edit edit, Reader reader) {
             if (edit.ClassroomToAdd != null) {
-                _context.Classroom.Add(new Classroom {
-                        Number = edit.ClassroomToAdd,
-                        Timestamp = DateTime.Now
-                        });
-                _context.SaveChanges();
+                try {
+                    _context.Classroom.Add(new Classroom {
+                            Number = edit.ClassroomToAdd,
+                            Timestamp = DateTime.Now
+                            });
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulAdding");
+                }
             }
             else if (edit.ClassroomToDelete != null) {
-                var _classrooms = from c in _context.Classroom
-                                where c.Number == edit.ClassroomToDelete
-                                select c;
-                var classroom = _classrooms.Single();
-                _context.Classroom.Remove(classroom);
-                _context.SaveChanges();
+                try {
+                    var _classrooms = from c in _context.Classroom
+                                    where c.Number == edit.ClassroomToDelete
+                                    select c;
+                    var classroom = _classrooms.Single();
+                    _context.Classroom.Remove(classroom);
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulDeleting");
+                }
             }
             else if (edit.ClassToAdd != null) {
-                _context.Class.Add(new Class {
-                        Name = edit.ClassToAdd,
-                        Timestamp = DateTime.Now
-                        });
-                _context.SaveChanges();
+                try {
+                    _context.Class.Add(new Class {
+                            Name = edit.ClassToAdd,
+                            Timestamp = DateTime.Now
+                            });
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulAdding");
+                }
             }
             else if (edit.ClassToDelete != null) {
-                var _classes = from c in _context.Class
-                                where c.Name == edit.ClassToDelete
-                                select c;
-                var _class = _classes.Single();
-                _context.Class.Remove(_class);
-                _context.SaveChanges();
+                try {
+                    var _classes = from c in _context.Class
+                                    where c.Name == edit.ClassToDelete
+                                    select c;
+                    var _class = _classes.Single();
+                    _context.Class.Remove(_class);
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulDeleting");
+                }
             }
             else if (edit.SubjectToAdd != null) {
-                _context.Subject.Add(new Subject {
-                        Name = edit.SubjectToAdd,
-                        Timestamp = DateTime.Now
-                        });
-                _context.SaveChanges();
+                try {
+                    _context.Subject.Add(new Subject {
+                            Name = edit.SubjectToAdd,
+                            Timestamp = DateTime.Now
+                            });
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulAdding");
+                }
             }
             else if (edit.SubjectToDelete != null) {
-                var _subjects = from s in _context.Subject
-                                where s.Name == edit.SubjectToDelete
-                                select s;
-                var subject = _subjects.Single();
-                _context.Subject.Remove(subject);
-                _context.SaveChanges();
+                try {
+                    var _subjects = from s in _context.Subject
+                                    where s.Name == edit.SubjectToDelete
+                                    select s;
+                    var subject = _subjects.Single();
+                    _context.Subject.Remove(subject);
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulDeleting");
+                }
             }
             else if (edit.TeacherToAdd != null) {
-                _context.Teacher.Add(new Teacher {
-                        Surname = edit.TeacherToAdd,
-                        Timestamp = DateTime.Now
-                        });
-                _context.SaveChanges();
+                try {
+                    _context.Teacher.Add(new Teacher {
+                            Surname = edit.TeacherToAdd,
+                            Timestamp = DateTime.Now
+                            });
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulAdding");
+                }
             }
             else if (edit.TeacherToDelete != null) {
-                var _teachers = from t in _context.Teacher
-                                where t.Surname == edit.TeacherToDelete
-                                select t;
-                var teacher = _teachers.Single();
-                _context.Teacher.Remove(teacher);
-                _context.SaveChanges();
+                try {
+                    var _teachers = from t in _context.Teacher
+                                    where t.Surname == edit.TeacherToDelete
+                                    select t;
+                    var teacher = _teachers.Single();
+                    _context.Teacher.Remove(teacher);
+                    _context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                    return RedirectToAction("UnsuccessfulDeleting");
+                }
             }
             var classes = _context.Class.OrderBy(c => c.Name).ToList();
             var classrooms = _context.Classroom.OrderBy(c => c.Number).ToList();
@@ -102,6 +142,14 @@ namespace SchoolPlanner.Controllers {
             ViewData["teachers"] = teachers; 
             ViewData["subjects"] = subjects;
             return View(edit);
+        }
+
+        public IActionResult UnsuccessfulAdding(Reader reader, int id) {
+            return View();
+        }
+
+        public IActionResult UnsuccessfulDeleting(Reader reader, int id) {
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
